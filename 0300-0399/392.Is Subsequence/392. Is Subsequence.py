31bf4d8e7d6e53132aee61ddff04f64a69626c57
Naim Moltrasio <naim.moltrasio@gmail.com>
2025-08-26 17:09:43 +0200

    feat: problem 11

---
+++ 0300-0399/392.Is Subsequence/392. Is Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+        j = 0
+        n = len(t)
+        for i in range(n):
+            if s[j] == t[i]:
+                j = j + 1
+                if j == len(s):
+                    return True
+        return False
