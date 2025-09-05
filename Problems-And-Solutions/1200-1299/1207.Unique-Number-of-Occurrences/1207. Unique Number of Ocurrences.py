97aade0c942c37c9bde711276cc2fe6eb1872152
Naim Moltrasio <naim.moltrasio@gmail.com>
2025-09-05 17:21:54 +0200

    refactor: cleaner solutions

--- Problems-And-Solutions/1200-1299/1207.Unique-Number-of-Occurrences/1207. Unique Number of Ocurrences.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def uniqueOccurrences(self, arr):
-        freq = {}
-        for x in arr:
-            freq[x] = freq.get(x, 0) + 1
-
-        return len(freq) == len(set(freq.values()))
